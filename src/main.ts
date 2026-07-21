f0754448dfc8a2a5d956a2379e3d4c3cbc2e3605
lochana2002 <ehltehelapitiya@gmail.com>
2026-07-21 23:41:16 +0530

    fix:fixed cors

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
  app.enableCors({
   origin: [
     "http://localhost:5173",
-    "https://safari-booking-idwg.vercel.app",
+    "https://safari-booking-suul.vercel.app",
   ],
   credentials: true,
 });
